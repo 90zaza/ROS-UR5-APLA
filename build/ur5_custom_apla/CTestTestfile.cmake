2574f81c436bd0ad42aed37ba7d5bc1b08437a84
Tim <timgeukers@planet.nl>
2025-01-31 10:37:41 +0100

    Cleaning up

--- build/ur5_custom_apla/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tim/FDMPrinting/src/ur5_custom_apla
-# Build directory: /home/tim/FDMPrinting/build/ur5_custom_apla
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/ur5_with_printhead/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tim/FDMPrinting/src/ur5_with_printhead
-# Build directory: /home/tim/FDMPrinting/build/ur5_with_printhead
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
