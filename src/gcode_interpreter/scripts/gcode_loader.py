3152e17e994df7af16db1420042934e861226acb
Tim <timgeukers@planet.nl>
2025-02-27 15:05:39 +0100

    Started pre-processing in main.py for toolpathPlanning

--- src/gcode_interpreter/scripts/gcode_loader.py
+++ src/gcode_interpreter/scripts/gcode_loader.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 import fdm_msgs.msg
 from std_msgs.msg import String
@@ -6,36 +8,45 @@ import sys
 import json
 
 class Communication:
-    def __init__(self, gcodeInterpreter):     
-        self.gcode_command_pub = rospy.Publisher('gcode_command', fdm_msgs.msg.GCodeCommand, queue_size=10)
+    def __init__(self):
+        self.gcodeInterpreter = None
 
+        self.gcode_command_pub = rospy.Publisher('/gcode_command', fdm_msgs.msg.GCodeCommand, queue_size=10)
+    
+    def set_class_pointers(self, gcodeInterpreter):
+        self.gcodeInterpreter = gcodeInterpreter
+
+        return
+
+    def set_subscribers(self):
+        rospy.Subscriber("gcode_raw", fdm_msgs.msg.GCode, self.gcodeInterpreter.line_interpreter)
+
+        return
+
+    def wait_for_publishers(self):
         while self.gcode_command_pub.get_num_connections() == 0:
             rospy.logwarn("Waiting for subscribers to connect on 'gcode_command'...")
             rospy.sleep(1)
 
         rospy.loginfo("Communication initialized for gcode_loader.py. Publishing to /gcode_command")
 
-        rospy.Subscriber("gcode_raw", fdm_msgs.msg.GCode, gcodeInterpreter.line_interpreter)
-
         return
-    
+
+
     def publish_gcode_command(self, command):
         self.gcode_command_pub.publish(command)
 
         return
 
 class GCodeInterpreter:
-    def __init__(self):
-        self.comms = None
+    def __init__(self, communication):
+        self.comms = communication
         self.seq_id = 0
         self.cmd_id = 1
         self.x_value = math.nan
         self.y_value = math.nan
         self.z_value = math.nan
         self.f_value = math.nan
-
-    def set_comms(self, communication):
-        self.comms = communication
 
     def line_interpreter(self, gcode_msg):
         for line in gcode_msg.lines:
@@ -92,9 +103,12 @@ class GCodeInterpreter:
 def main():
     rospy.init_node('gcode_interpreter', anonymous=True)
 
-    gcodeInterpreter = GCodeInterpreter()
-    comms = Communication(gcodeInterpreter)
-    gcodeInterpreter.set_comms(comms)
+    comms = Communication()
+    gcodeInterpreter = GCodeInterpreter(comms)
+
+    comms.set_class_pointers(gcodeInterpreter)
+    comms.set_subscribers()
+    comms.wait_for_publishers()
     
     rospy.spin()
 
